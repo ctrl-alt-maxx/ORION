c9cb466d0b0323cc095db918ec54b6ab9e89cf9d
AurelienLeFrancais <eloy.aur@gmail.com>
2023-04-11 10:35:50 -0400

    xxx

--- 2023_Orion_22_janvier/Orion_client/orion_vue.py
+++ 2023_Orion_22_janvier/Orion_client/orion_vue.py
@@ -108,6 +108,7 @@ class Vue():
         self.cadre_construire_entrepot = Frame()
         self.cadre_construire_usine = Frame()
         self.cadre_bouton_transferer = Frame()
+        self.cadre_choisir_transfere = Frame()
         self.chiffre = 0
         self.nbr_entrepot = 0
         self.selectedTags = None
@@ -132,12 +133,69 @@ class Vue():
     def faireApparaitreBoutonTransfere(self):
         self.cadre_bouton_transferer = Frame(self.cadreoutils ,width=200, height=200)
         self.cadre_bouton_transferer.pack()
-        self.bouton = Button(self.cadre_bouton_transferer, text="bouton transfere")
+        self.bouton = Button(self.cadre_bouton_transferer, text="bouton transfere",command=self.choisir_transfere)
         self.bouton.pack()
-
-
         #creation des labels autres
         self.timer_partie = Label()
+
+    def choisir_transfere(self):#methode quand on clique sur le bouton transferer qui affiche le menu permettant de transférer les ressources de l'inventaire de l'étoile à l'inventaire du cargo
+        self.cadre_bouton_transferer.pack_forget()
+        self.cadre_choisir_transfere = Frame(self.cadreoutils, width=400, height=200, bg="red")
+
+        self.cadre_quantite_fer = Frame(self.cadre_choisir_transfere, width=200, height=200)
+        self.label_qtite_fer = Label(self.cadre_quantite_fer,text="Fer:")
+        self.label_chiffre_fer = Label(self.cadre_quantite_fer,text="50")
+
+        self.cadre_entre_qtite_fer = Frame(self.cadre_choisir_transfere, width=400, height=200)
+        self.label_entrez_qtite = Label(self.cadre_entre_qtite_fer, text="Entrez une quantite:")
+        self.label_qtiteFer_entre = Label(self.cadre_entre_qtite_fer, text="100")
+
+        self.cadre_quantite_fer.pack()
+        self.label_qtite_fer.pack(side=LEFT)
+        self.label_chiffre_fer.pack()
+
+        # self.cadre_entre_qtite_fer.pack(fill=X)
+        # self.label_entrez_qtite.pack(side=LEFT)
+        # self.label_qtiteFer_entre.pack()
+
+        self.cadre_quantite_fer = Frame(self.cadre_choisir_transfere, width=200, height=200)
+        self.label_qtite_fer = Label(self.cadre_quantite_fer, text="Fer:")
+        self.label_chiffre_fer = Label(self.cadre_quantite_fer, text="50")
+
+        self.cadre_entre_qtite_fer = Frame(self.cadre_choisir_transfere, width=400, height=200)
+        self.label_entrez_qtite_fer = Label(self.cadre_entre_qtite_fer, text="Entrez une quantite:")
+        self.label_qtiteFer_entre = Label(self.cadre_entre_qtite_fer, text="100")
+
+        self.cadre_quantite_fer.pack()
+        self.label_qtite_fer.pack(side=LEFT)
+        self.label_chiffre_fer.pack()
+
+        # self.cadre_entre_qtite_fer.pack(fill=X)
+        # self.label_entrez_qtite_fer.pack(side=LEFT)
+        # self.label_qtiteFer_entre.pack()
+
+        #FAIRE cuivre
+        # self.cadre_quantite_cuivre = Frame(self.cadre_choisir_transfere, width=200, height=200)
+        # self.label_qtite_cuivre = Label(self.cadre_quantite_fer, text="Cuivre:")
+        # self.label_chiffre_cuivre = Label(self.cadre_quantite_fer, text="50")
+        #
+        # self.cadre_entre_qtite_cuivre = Frame(self.cadre_choisir_transfere, width=400, height=200)
+        # self.label_entrez_qtite_cuivre = Label(self.cadre_entre_qtite_fer, text="Entrez une quantite:")
+        # self.label_qtiteCuivre_entre = Label(self.cadre_entre_qtite_fer, text="100")
+        #
+        # self.cadre_quantite_cuivre.pack()
+        # self.label_qtite_cuivre.pack(side=LEFT)
+        # self.label_chiffre_cuivre.pack()
+        #
+        # self.cadre_entre_qtite_cuivre.pack(fill=X)
+        # self.label_entrez_qtite_cuivre.pack(side=LEFT)
+        # self.label_qtiteCuivre_entre.pack()
+
+        self.cadre_choisir_transfere.pack()
+
+        #FAIRE LE MENU ICI -> dans cadre_choisir_transfere
+
+
 
     def clock(self):#des que je clique sur le bouton construire entrepot, ca lance cette fonction...
         self.chiffre += 1
@@ -538,7 +596,7 @@ class Vue():
 
 
 
-    def menu_ressource(self):# on arrive ici quand on clique sur le bouton "Ressources"
+    def menu_ressource(self):# on arrive ici quand on clique sur le bouton "Inventaire"
             self.recup = self.parent.recupEtoile(self.ma_selection[1])
             self.forget_all()
 
@@ -816,6 +874,7 @@ class Vue():
         self.clickUneFoisSurRessource = 0
         self.clickUneFoisSurVaiss += 1
         self.clickUneFoisSurInsta = 0
+        print("Efface")
 
         if self.clickUneFoisSurVaiss == 1:
             self.boutonConstruireUsine.pack_forget()
@@ -833,6 +892,8 @@ class Vue():
             self.label_hydrogene.pack_forget()
             self.btnInstallation.pack_forget()
             self.btnInventaire.pack_forget()
+
+
 
         self.barrevie.value = objet.Vie
 
@@ -959,6 +1020,7 @@ class Vue():
                     #enlever le menu du haut ici quand on clique sur le vaisseau car on ne veut plus savoir ce quil y a sur l'étoile
                     self.cadreinfochoix.pack_forget()
                     self.cadre_bouton_transferer.pack_forget()
+                    self.cadre_menu_ressource.pack_forget()
                     #si je clique sur le cargot
                     if(self.type_vaisseau_selectionne == "Cargo"):
                         if(self.cargoArrive):#si il est accoste
