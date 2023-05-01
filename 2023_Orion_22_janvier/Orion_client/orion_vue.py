065d164b8efd330dbbd6c9dab7d487b0d4a99903
maxx <98351883+ctrl-alt-maxx@users.noreply.github.com>
2023-05-01 10:06:51 -0400

    k

--- 2023_Orion_22_janvier/Orion_client/orion_vue.py
+++ 2023_Orion_22_janvier/Orion_client/orion_vue.py
@@ -700,7 +700,7 @@ class Vue():
             #Cadre et Label entrepot
             self.cadre_img2 = Frame(self.cadre_menu_installation, height=200, width=200,background="#DCE0D9")#dans cadre img2 je met image + descritpion
             self.cadre_img2.pack(fill=X)
-            self.label_img2 = Label(self.cadre_img2, image=self.images["entrepot3"],bg="#DCE0D9")
+            self.label_img2 = Label(self.cadre_img2, image=self.images["entrepot"],bg="#DCE0D9")
             self.label_img2.pack(side=LEFT)
             self.label_installation2 = Label(self.cadre_img2, text="Description: Entrepot pour construire vaisseaux",bg="#DCE0D9")
             self.label_installation2.pack()
